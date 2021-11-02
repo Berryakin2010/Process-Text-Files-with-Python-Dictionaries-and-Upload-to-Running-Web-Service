a209c030d83116cf1b64bd8d3f28538e3059a5f9
Akin Berry <69063809+Berryakin2010@users.noreply.github.com>
2021-11-02 16:52:19 -0700

    IP address source
    Same IP address used to login to SSH PPK.

--- run.py
+++ run.py
@@ -13,5 +13,5 @@ for filename in os.listdir(directory):
             date = lines[2].strip()
             feedback = lines[3].strip()
             data = {"title": title, "name": name, "date": date, "feedback": feedback}
-            res = requests.post("http://<IP address>/feedback/", data=data)
+            res = requests.post("http://<IP address used to login through SSH PPK>/feedback/", data=data)
             print("status_code ",res.status_code)
